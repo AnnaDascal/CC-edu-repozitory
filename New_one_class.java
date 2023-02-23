ec81b7842dc691c8823e8e77ce1ddc6186d6feef
Anna Dascal <aniutik95@gmail.com>
2023-02-23 12:39:03 +0200

    New commit2

--- New_one_class.java
+++ New_one_class.java
@@ -1,2 +1,21 @@
 public class New_one_class {
+
+        public static void main(String[] args) {
+            //напишите тут ваш код
+            Scanner s = new Scanner(System.in);
+            String s1 = s.nextLine();
+            String s2 = s1.toLowerCase();
+
+            Scanner c = new Scanner ( System.in);
+            String c1 = c.nextLine();
+            String c2 = c1.toUpperCase();
+
+            Scanner a = new Scanner ( System.in);
+            String a1 = a.nextLine();
+
+            System.out.println(a1);
+            System.out.println(c2);
+            System.out.println(s2);
+        }
+    }
 }
